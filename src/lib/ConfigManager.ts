f4cacfc2ad4444d7d26b2aa364abfd59e82bac96
Clément Chausson <clement.chausson@epitech.eu>
2024-04-10 19:35:06 +0200

    [+] DELETE and GET for server config

--- src/lib/ConfigManager.ts
+++ src/lib/ConfigManager.ts
@@ -1,10 +1,14 @@
-import { env } from '$env/dynamic/public'; 
+import { env } from '$env/dynamic/public';
 
 export class ConfigManager {
     static getConfig() {
+        let templateConfig = env.PUBLIC_CONF_TEMPLATE
+            ? Buffer.from(env.PUBLIC_CONF_TEMPLATE, 'base64').toString('utf-8')
+            : 'defaultTemplateConfig'; // replace with your default value
         return {
             resourceFolder: env.PUBLIC_RESOURCES_FOLDER || '/home/beam/server/Resources',
-            serverConfig: env.PUBLIC_CONFG_PATH || '/home/beam/server/ServerConfig.toml',
+            serverConfig: env.PUBLIC_CONF_PATH || '/home/beam/server/ServerConfig.toml',
+            templateConfig: templateConfig,
         };
     }
 }
---
+++ src/routes/api/config/+server.ts
@@ -0,0 +1,58 @@
+import { ConfigManager } from '$lib/ConfigManager';
+import { writeFile, readFile } from 'node:fs/promises';
+
+
+/** @type {import('./$types').RequestHandler} */
+export async function DELETE() {
+    // this will reset the config with the template
+    const config = ConfigManager.getConfig();
+    try {
+        await writeFile(config.serverConfig, config.templateConfig);
+    } catch (err) {
+        console.error(`Error writing to file ${config.serverConfig}:`, err);
+        return new Response(JSON.stringify({
+            success: false
+        }), {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            status: 500
+        });
+    }
+    return new Response(JSON.stringify({
+        success: true
+    }), {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        status: 200
+    });
+}
+
+export async function GET() {
+
+    let content;
+    try {
+        content = await readFile(ConfigManager.getConfig().serverConfig);
+    } catch {
+        return new Response(JSON.stringify({
+            success: false
+        }), {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            status: 500
+        });
+    }
+
+    return new Response(content, {
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        status: 200
+    });
+}
+
+export async function POST() {
+    
+}
